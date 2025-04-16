1abbf74b81f4e2fc8d2e594030c8d1c13348b695
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-04-17 04:19:29 +0900

    config関連

--- src/main/java/eng/pro/yui/mcpl/moveAsYou/config/MoveAsYouConfig.java
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/config/MoveAsYouConfig.java
@@ -1,7 +1,34 @@
 package eng.pro.yui.mcpl.moveAsYou.config;
 
-public class MoveAsYouConfig {
+import eng.pro.yui.mcpl.moveAsYou.MoveAsYou;
+import org.bukkit.configuration.file.FileConfiguration;
+
+public final class MoveAsYouConfig {
     
+    // config values
+    
+    /** WebServerの実行port */
+    public static int webPort;
+    
+    
+    // methods
+    
+    /** 
+     * config.ymlから値を読み込みます。
+     * 
+     * 指定が無い場合既定値とします
+     * */
     public static void load(){
+        
+        FileConfiguration configFile = MoveAsYou.plugin().getConfig();
+        
+        webPort = 38080;
+        if(configFile.contains("webPort")){
+            webPort = configFile.getInt("webPort");
+        }
+        
+        MoveAsYou.log().info("Config file loaded. Values are below: -->");
+        MoveAsYou.log().info("webPort: " + webPort);
+        MoveAsYou.log().info("<-- config.");
     }
 }
