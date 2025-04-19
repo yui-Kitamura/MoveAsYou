6a0adfb206db99794434fd7100f5ce203a04fab3
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-04-20 00:25:14 +0900

    htmlハンドリングの処理追加

--- src/main/java/eng/pro/yui/mcpl/moveAsYou/web/MAYHttpHandler.java
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/web/MAYHttpHandler.java
@@ -2,20 +2,16 @@ package eng.pro.yui.mcpl.moveAsYou.web;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import eng.pro.yui.mcpl.moveAsYou.exception.RuntimeMAYException;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
 
 public class MAYHttpHandler implements HttpHandler {
     
     public static final String PATH = "/";
-    private final File rootIndex;
+    private final String rootIndex = "index";
     
     public MAYHttpHandler(){
-        this.rootIndex = new File("webroot/index.html");
+        //nothing to do
     }
     
     @Override
@@ -24,17 +20,14 @@ public class MAYHttpHandler implements HttpHandler {
             String requestPath = exchange.getRequestURI().getPath();
             String resStr;
             if(PATH.equals(requestPath)){
-                resStr = Files.readString(rootIndex.toPath());
-                exchange.sendResponseHeaders(200, resStr.getBytes().length);
+                String html = WebServer.getRenderer().render(rootIndex, null);
+                WebServer.send(200, html, exchange);
             }else{
                 resStr = "404 not found";
-                exchange.sendResponseHeaders(404, resStr.getBytes().length);
+                WebServer.send(404, resStr, exchange);
             }
-            OutputStream out = exchange.getResponseBody();
-            out.write(resStr.getBytes());
-            out.close();
         }catch(IOException e){
-            throw new RuntimeMAYException(e);
+            WebServer.send(e, exchange);
         }
     }
 }
--- src/main/java/eng/pro/yui/mcpl/moveAsYou/web/PlayerNameHandler.java
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/web/PlayerNameHandler.java
@@ -2,16 +2,45 @@ package eng.pro.yui.mcpl.moveAsYou.web;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import eng.pro.yui.mcpl.moveAsYou.MoveAsYou;
+import org.bukkit.entity.Player;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PlayerNameHandler implements HttpHandler {
 
     public static final String PATH = "/p/";
-    
+    private static final String playerTemplate = "player";
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String pathPlayerName = exchange.getRequestURI().getPath().substring(PATH.length());
+        String calledPath = exchange.getRequestURI().getPath();
+        if(!calledPath.startsWith(PATH) || calledPath.length() == PATH.length() ) {
+            WebServer.send(400, "PlayerName is missed", exchange);
+            return;
+        }
         
+        String pathPlayerName = exchange.getRequestURI().getPath().substring(PATH.length());
+
+        Player player = MoveAsYou.plugin().getServer().getPlayer(pathPlayerName);
+        
+        try {
+            if (player == null) {
+                WebServer.send(400, "Player not found", exchange);
+                return;
+            }
+
+            Map<String, Object> variables = new HashMap<>();
+            variables.put("playerName", pathPlayerName);
+            variables.put("playerUid", player.getUniqueId().toString());
+
+            String html = WebServer.getRenderer().render(playerTemplate, variables);
+            WebServer.send(200, html, exchange);
+
+        }catch(IOException ioe) {
+            WebServer.send(ioe, exchange);
+        }
     }
 }
