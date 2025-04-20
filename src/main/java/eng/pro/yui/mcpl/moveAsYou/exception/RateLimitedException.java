2bba213268f36a87fff836d69882bce0bd36cf5f
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-04-20 21:17:48 +0900

    例外の追加

---
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/exception/RateLimitedException.java
@@ -0,0 +1,7 @@
+package eng.pro.yui.mcpl.moveAsYou.exception;
+
+public class RateLimitedException extends RuntimeMAYException {
+    public RateLimitedException(String message) {
+        super(message);
+    }
+}
