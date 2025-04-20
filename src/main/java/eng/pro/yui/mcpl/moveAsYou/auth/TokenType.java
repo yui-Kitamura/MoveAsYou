a16a8320ebde5713eafb895f2b11e0a20551b2db
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-04-20 18:06:08 +0900

    ドメイン情報を自身で持つリファクタ

--- src/main/java/eng/pro/yui/mcpl/moveAsYou/auth/TokenType.java
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/auth/TokenType.java
@@ -2,11 +2,19 @@ package eng.pro.yui.mcpl.moveAsYou.auth;
 
 public enum TokenType {
     /** 使い捨て 60分有効 */
-    ONE_TIME,
+    ONE_TIME(false),
     /** 配信者向け 自動延長 */
-    STREAMING,
+    STREAMING(true),
     /** 管理者向け 無期限 */
-    ADMIN;
+    ADMIN(false);
     
     public final static long defaultExpireTime = 60*60*1_000; //1h 
+    
+    public final boolean isExpireTimeUpdatable;
+    
+    TokenType(boolean isExpireTimeUpdatable) {
+        this.isExpireTimeUpdatable = isExpireTimeUpdatable;
+    }
+    
+    
 }
