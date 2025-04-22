ec6349ce6508c16bab09855c75e468a26ae2e2d6
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-04-22 22:18:55 +0900

    nameとaliasの拡張

--- src/main/java/eng/pro/yui/mcpl/moveAsYou/auth/TokenType.java
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/auth/TokenType.java
@@ -1,5 +1,10 @@
 package eng.pro.yui.mcpl.moveAsYou.auth;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public enum TokenType {
     /** 使い捨て 60分有効 */
     ONE_TIME(false),
@@ -16,5 +21,31 @@ public enum TokenType {
         this.isExpireTimeUpdatable = isExpireTimeUpdatable;
     }
     
+    /** 正式名もしくはaliasからTokenTypeを取得します */
+    public static TokenType get(String nameOrAlias){
+        if(nameOrAlias == null){ return null; }
+        try {
+            return TokenType.valueOf(nameOrAlias.toUpperCase());
+        }catch(IllegalArgumentException e){
+            switch(nameOrAlias.toLowerCase()){
+                case "onetime":
+                    return TokenType.ONE_TIME;
+                case "stream":
+                    return TokenType.STREAMING;
+                default:
+                    return null;
+            }
+        }
+   }
+
+   public static @NotNull List<String> names(){
+        List<String> list = new ArrayList<String>();
+        list.add(TokenType.ONE_TIME.name());
+        list.add(TokenType.STREAMING.name());
+        list.add(TokenType.ADMIN.name());
+        list.add("onetime");
+        list.add("stream");
+        return list;
+   }
     
 }
