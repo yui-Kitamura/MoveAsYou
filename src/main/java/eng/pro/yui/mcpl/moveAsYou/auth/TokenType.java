4e122450019e3eb923133c666682a5f6b769c35f
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-04-22 22:27:13 +0900

    nameとaliasの拡張と実装

--- src/main/java/eng/pro/yui/mcpl/moveAsYou/auth/TokenType.java
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/auth/TokenType.java
@@ -7,34 +7,36 @@ import java.util.List;
 
 public enum TokenType {
     /** 使い捨て 60分有効 */
-    ONE_TIME(false),
+    ONE_TIME(false, "onetime"),
     /** 配信者向け 自動延長 */
-    STREAMING(true),
+    STREAMING(true, "stream"),
     /** 管理者向け 無期限 */
-    ADMIN(false);
+    ADMIN(false, "admin");
     
     public final static long defaultExpireTime = 60*60*1_000; //1h 
     
     public final boolean isExpireTimeUpdatable;
+    public final String alias;
     
-    TokenType(boolean isExpireTimeUpdatable) {
+    TokenType(boolean isExpireTimeUpdatable, String alias) {
         this.isExpireTimeUpdatable = isExpireTimeUpdatable;
+        this.alias = alias;
     }
     
     /** 正式名もしくはaliasからTokenTypeを取得します */
     public static TokenType get(String nameOrAlias){
         if(nameOrAlias == null){ return null; }
         try {
+            //nameでの判定
             return TokenType.valueOf(nameOrAlias.toUpperCase());
         }catch(IllegalArgumentException e){
-            switch(nameOrAlias.toLowerCase()){
-                case "onetime":
-                    return TokenType.ONE_TIME;
-                case "stream":
-                    return TokenType.STREAMING;
-                default:
-                    return null;
+            String lowerInput = nameOrAlias.toLowerCase();
+            for(TokenType t : TokenType.values()){
+                if(t.alias.equals(lowerInput)){
+                    return t;
+                }
             }
+            return null;
         }
    }
 
@@ -43,8 +45,9 @@ public enum TokenType {
         list.add(TokenType.ONE_TIME.name());
         list.add(TokenType.STREAMING.name());
         list.add(TokenType.ADMIN.name());
-        list.add("onetime");
-        list.add("stream");
+        list.add(TokenType.ONE_TIME.alias);
+        list.add(TokenType.STREAMING.alias);
+        list.add(TokenType.ADMIN.alias); //小文字なので区別して返す
         return list;
    }
     
