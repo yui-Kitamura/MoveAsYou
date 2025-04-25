39a8ada0ee7552e416998f78fc2ec53e0505d829
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-04-25 20:50:53 +0900

    適切な型の利用

--- src/main/java/eng/pro/yui/mcpl/moveAsYou/web/data/AuthRequestInfo.java
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/web/data/AuthRequestInfo.java
@@ -1,6 +1,8 @@
 package eng.pro.yui.mcpl.moveAsYou.web.data;
 
+import eng.pro.yui.mcpl.moveAsYou.auth.TokenText;
+
 public class AuthRequestInfo {
-    public String token;
+    public TokenText token;
     public String playerName;
 }
