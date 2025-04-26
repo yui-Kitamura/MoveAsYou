1ce0c7e50bfc3c4d9da1b3d411e7df39f153aecd
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-04-27 00:48:02 +0900

    基礎overrideの実装

--- src/main/java/eng/pro/yui/mcpl/moveAsYou/web/data/AuthRequestInfo.java
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/web/data/AuthRequestInfo.java
@@ -5,4 +5,31 @@ import eng.pro.yui.mcpl.moveAsYou.auth.TokenText;
 public class AuthRequestInfo {
     public TokenText token;
     public String playerName;
+    
+    public AuthRequestInfo(TokenText token, String playerName) {
+        this.token = token;
+        this.playerName = playerName;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 17;
+        hash = hash * 31 + token.hashCode();
+        hash = hash * 31 + playerName.hashCode();
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) { return true; }
+        if (obj == null) { return false; }
+        if (obj.getClass() != this.getClass()) { return false; }
+        AuthRequestInfo other = (AuthRequestInfo) obj;
+        return token.equals(other.token) && playerName.equals(other.playerName);
+    }
+
+    @Override
+    public String toString() {
+        return "AuthRequestInfo{token=" + token + ", playerName=" + playerName + "}";
+    }
 }
