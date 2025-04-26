6c2ab674fd4f36324c7c7dd08d830465681d0058
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-04-27 02:17:35 +0900

    HtmlText型の作成

---
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/web/HtmlText.java
@@ -0,0 +1,51 @@
+package eng.pro.yui.mcpl.moveAsYou.web;
+
+import eng.pro.yui.mcpl.moveAsYou.MoveAsYou;
+import eng.pro.yui.mcpl.moveAsYou.config.MoveAsYouConfig;
+
+public class HtmlText {
+
+    private final String rawHtml;
+    public String getRawHtml() {
+        return rawHtml;
+    }
+
+    public HtmlText(String rawHtml) {
+        if (rawHtml == null || rawHtml.isBlank()) {
+            throw new IllegalArgumentException("HTML text cannot be null or empty.");
+        }
+        this.rawHtml = rawHtml;
+    }
+    
+    public static HtmlText getFull(String rawHtml){
+        return new HtmlText(rawHtml);
+    }
+    public static HtmlText get(String title, String body){
+        return new HtmlText(
+                "<!DOCTYPE html><html><title>"+title+"|"+ MoveAsYouConfig.webTitle +"</title>" +
+                "<body>"+body+"</body></html>");
+    }
+    public static HtmlText getWithBody(String body){
+        return new HtmlText("<!DOCTYPE html><html><title>"+ MoveAsYouConfig.webTitle +"</title>" +
+                "<body>"+body+"</body></html>");
+    }
+
+    @Override
+    public String toString() {
+        return rawHtml;
+    }
+
+    @Override
+    public int hashCode() {
+        return rawHtml.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this){ return true; }
+        if(obj == null){ return false; }
+        if(obj.getClass() != this.getClass()){ return false; }
+        HtmlText other = (HtmlText) obj;
+        return rawHtml.equals(other.rawHtml);
+    }
+}
