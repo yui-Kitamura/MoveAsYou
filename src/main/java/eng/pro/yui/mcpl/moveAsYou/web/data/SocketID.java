31e2359c8c1b1905b03625204411af1dda7f84c9
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-04-28 17:45:19 +0900

    SocketID Stringラッパーの作成

---
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/web/data/SocketID.java
@@ -0,0 +1,44 @@
+package eng.pro.yui.mcpl.moveAsYou.web.data;
+
+import org.java_websocket.WebSocket;
+import java.util.Arrays;
+
+public class SocketID {
+    
+    private byte[] id;
+
+    // constructor
+    public SocketID(byte[] id) {
+        this.id = id;
+    }
+    public SocketID(WebSocket socket){
+        this.id = socket.getSSLSession().getId();
+    }
+    
+    // methods
+    public byte[] getIdArray(){
+        return id;
+    }
+    public String getIdValue(){
+        return new String(id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null){ return false; }
+        if(obj.getClass().equals(this.getClass())){
+            return this.getIdValue().equals(((SocketID)obj).getIdValue());            
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return getIdValue();
+    }
+}
