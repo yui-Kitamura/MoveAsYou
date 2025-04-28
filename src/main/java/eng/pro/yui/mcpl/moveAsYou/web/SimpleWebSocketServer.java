a4328f7e987ebc4c75a41f9af35d654565df1123
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-04-28 18:17:55 +0900

    socketサーバの実装

--- src/main/java/eng/pro/yui/mcpl/moveAsYou/web/SimpleWebSocketServer.java
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/web/SimpleWebSocketServer.java
@@ -2,18 +2,29 @@ package eng.pro.yui.mcpl.moveAsYou.web;
 
 import eng.pro.yui.mcpl.moveAsYou.MoveAsYou;
 
+import eng.pro.yui.mcpl.moveAsYou.auth.TokenText;
+import eng.pro.yui.mcpl.moveAsYou.mc.data.PlayerName;
+import eng.pro.yui.mcpl.moveAsYou.web.data.PlayerInfo;
+import eng.pro.yui.mcpl.moveAsYou.web.data.SocketID;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SimpleWebSocketServer extends WebSocketServer {
+    
+    private static final String playerListenerPath = "/socket/player/";
+    
+    private final Map<SocketID, PlayerName> connectionAndPlayer;
 
     public SimpleWebSocketServer(int port) {
         super(new InetSocketAddress(InetAddress.getLoopbackAddress(), port));
+        connectionAndPlayer = new HashMap<>();
     }
     
     @Override
@@ -23,9 +34,20 @@ public class SimpleWebSocketServer extends WebSocketServer {
     
     @Override
     public void onOpen(WebSocket con, ClientHandshake handshake) {
-        MoveAsYou.log().info("WebSocket connected!");
-        //TODO implement auth phase
-    
+        String endPoint = handshake.getResourceDescriptor();
+        if(endPoint.startsWith(playerListenerPath) == false){
+            con.close(4004, "endpoint missed");
+            MoveAsYou.log().info("wrong Socket request:" + endPoint);
+            return;
+        }
+        if(endPoint.length() == playerListenerPath.length()){
+            con.close(4001, "parameter missed");
+            return;
+        }
+        PlayerName playerName = new PlayerName(endPoint.substring(playerListenerPath.length()));
+        connectionAndPlayer.put(new SocketID(con), playerName);
+
+        MoveAsYou.log().info("WebSocket connected! ("+ playerName + ")");
     }
 
     @Override
@@ -40,7 +62,31 @@ public class SimpleWebSocketServer extends WebSocketServer {
     
     @Override
     public void onMessage(WebSocket con, String message) {
-        con.send("on message");
+        MoveAsYou.log().info("Received message: " + message);
+        if(message.startsWith("Token:")) {
+            onMessageToken(con, message);
+            return;
+        }
+        con.send("server doing nothing for your message");
+    }
+    
+    private void onMessageToken(WebSocket con, String message){
+        String token = message.substring("Token:".length());
+        TokenText t = new TokenText(token);
+        PlayerName pn = connectionAndPlayer.get(new SocketID(con));
+        MoveAsYou.tokenManager().validate(t, pn);
+    }
+    
+    public void sendPlayerInfo(){
+        for(WebSocket con : getConnections()){
+            SocketID key = new SocketID(con);
+            PlayerInfo pi = MoveAsYou.playerMonitor().get(connectionAndPlayer.get(key));
+            if(pi == null) {
+                con.close(4001, "player disconnected");
+                return;
+            }
+            if(con.isOpen()){ con.send(pi.toJsonString()); }
+        }
     }
         
 }
