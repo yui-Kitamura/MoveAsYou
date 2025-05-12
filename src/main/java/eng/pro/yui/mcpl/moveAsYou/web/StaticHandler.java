d65572fc5ffa1e367cdcf0d57565a786e55501ad
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-05-12 18:10:13 +0900

    import整理

--- src/main/java/eng/pro/yui/mcpl/moveAsYou/web/StaticHandler.java
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/web/StaticHandler.java
@@ -7,7 +7,6 @@ import eng.pro.yui.mcpl.moveAsYou.MoveAsYou;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
