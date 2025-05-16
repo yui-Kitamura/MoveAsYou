235d08f636c0477c6ac780d1e56b6d8c09eca37a
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-05-16 20:10:55 +0900

    permissionの宣言

--- src/main/java/eng/pro/yui/mcpl/moveAsYou/consts/Permissions.java
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/consts/Permissions.java
@@ -20,6 +20,10 @@ public final class Permissions {
     public static Permission LIST_ADMIN;
     /** 管理者向け トークンの全体状況把握 */
     public static Permission STATS_ADMIN;
+    /** 自身の背景色設定 */
+    public static Permission CONFIG_SELF_BGCOLOR;
+    /** 他プレイヤー分の背景色設定 */
+    public static Permission CONFIG_ADMIN_BGCOLOR;
     
     static {
         List<Permission> list = MoveAsYou.plugin().getDescription().getPermissions();
@@ -37,6 +41,10 @@ public final class Permissions {
                     LIST_ADMIN = p; break;
                 case "moveAsYou.stats.admin":
                     STATS_ADMIN = p; break;
+                case "moveAsYou.config.self.bgcl":
+                    CONFIG_SELF_BGCOLOR = p; break;
+                case "moveAsYou.config.admin.bgcl":
+                    CONFIG_ADMIN_BGCOLOR = p; break;
                 default:
                     throw new IllegalArgumentException("unexpected permission declared:" + p.getName());
             }
