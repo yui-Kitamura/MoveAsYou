d8a842dbf5e42fc4241eb53ce8c961cc4a6b5e1f
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-05-18 04:00:44 +0900

    testの追加

--- src/test/java/eng/pro/yui/mcpl/moveAsYou/consts/BgColorTest.java
+++ src/test/java/eng/pro/yui/mcpl/moveAsYou/consts/BgColorTest.java
@@ -2,6 +2,8 @@ package eng.pro.yui.mcpl.moveAsYou.consts;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BgColorTest {
@@ -17,6 +19,15 @@ class BgColorTest {
     @Test
     void runGetByInt(){
         assertEquals(BgColor.RED, BgColor.get(0xff0000));
+    }
+    
+    @Test
+    void nameList(){
+        List<String> names = BgColor.names();
+        assertAll(
+                ()->assertFalse(names.isEmpty()),
+                ()->assertEquals(BgColor.values().length, names.size())
+        );
     }
 
 }
