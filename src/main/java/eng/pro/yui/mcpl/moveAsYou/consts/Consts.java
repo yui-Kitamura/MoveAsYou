8280787015cbd804a6b157859a88f89920270742
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-05-24 17:56:50 +0900

    装飾表示の定数化

--- src/main/java/eng/pro/yui/mcpl/moveAsYou/consts/Consts.java
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/consts/Consts.java
@@ -1,8 +1,16 @@
 package eng.pro.yui.mcpl.moveAsYou.consts;
 
+import org.bukkit.ChatColor;
+
 public final class Consts {
     private Consts(){ /* インスタンス化禁止 */ }
 
     /** Minecraftチャット欄の改行文字 */
     public static final String br = "\n";
+    
+    /** MoveAsYou装飾テキスト */
+    public static final String MAYDecorated =
+            "" + ChatColor.RESET + ChatColor.ITALIC +
+            ChatColor.BLUE + "Move" + ChatColor.RED + "As" + ChatColor.GREEN + "You" + 
+            ChatColor.RESET;
 }
