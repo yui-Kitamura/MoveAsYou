00f0ec1cabb6c73652a90eae55aace6842dbf71d
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-08 05:40:02 +0900

    フロントに返すデータ構造

---
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/web/data/IndexPlayer.java
@@ -0,0 +1,10 @@
+package eng.pro.yui.mcpl.moveAsYou.web.data;
+
+import eng.pro.yui.mcpl.moveAsYou.mc.data.PlayerName;
+
+public class IndexPlayer {
+    
+    PlayerName mcid;
+    String skinUrl;
+    String onlineFor;
+}
