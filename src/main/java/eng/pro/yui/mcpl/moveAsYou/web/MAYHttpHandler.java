b71bd460d935a9670757e783684cdb4efbd7f3b4
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-08 09:14:30 +0900

    ユーザー表示部とスキン表示機能の追加

--- src/main/java/eng/pro/yui/mcpl/moveAsYou/web/MAYHttpHandler.java
+++ src/main/java/eng/pro/yui/mcpl/moveAsYou/web/MAYHttpHandler.java
@@ -2,10 +2,13 @@ package eng.pro.yui.mcpl.moveAsYou.web;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import eng.pro.yui.mcpl.moveAsYou.MoveAsYou;
 import eng.pro.yui.mcpl.moveAsYou.web.data.HtmlText;
+import eng.pro.yui.mcpl.moveAsYou.web.data.IndexPlayer;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MAYHttpHandler implements HttpHandler {
     
@@ -31,9 +34,19 @@ public class MAYHttpHandler implements HttpHandler {
     
     private HtmlText renderIndex(HttpExchange exchange){
         Map<String, Object> variables = new HashMap<>();
-
-
+        variables.put("users", getPlayerList());
         return HtmlText.getFull(WebServer.getRenderer().render(rootIndex, variables));
+    }
+    
+    private List<IndexPlayer> getPlayerList(){
+        long current = System.currentTimeMillis();
+        Collection<? extends Player> all = MoveAsYou.plugin().getServer().getOnlinePlayers();
+        
+        List<IndexPlayer> result = new ArrayList<>();
+        for(Player p : all) {
+            IndexPlayer ip = new IndexPlayer(p, current);
+        }
+        return result;
     }
     
 }
